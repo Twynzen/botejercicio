7a8bcf57ad9544872335cd0883feb7f7940d8197
Raziel <razzziel001@gmail.com>
2019-06-02 15:24:50 -0500

    trato de que le bot responda ping con un pong

--- index.js
+++ index.js
@@ -4,8 +4,19 @@ const client = new Discord.Client();
 
 client.on('ready', ()=>{
 
-    console.log('El bot esta listo como ${client.user.tag}');
+    console.log(`El bot esta listo como:  ${client.user.tag}`);
 
 });
 
-client.login('NTg0MjM5NTIxMTI2Njc4NTUw.XPICzg.OdUbaIiH-K4yuEZQLS6brVwJ620');
+client.on('message', message => {
+    // recibiendo el mensaje
+    console.log(message.content);
+
+   if (message.content === 'ping'){
+       message.reply('pong');
+   }
+   
+});
+
+
+client.login('NTg0MjM5NTIxMTI2Njc4NTUw.XPQr9w.FWzwGPkpbxd2U39DpVPPGV7xbZA');
